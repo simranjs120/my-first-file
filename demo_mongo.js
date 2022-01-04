c9934ac1aa0ada6c6819f64dabe6f0894471df4e
Simranjeet Singh <simranjs5041@gmail.com>
2022-01-04 16:32:50 +0530

    added hello

--- demo_mongo.js
+++ demo_mongo.js
@@ -3,7 +3,7 @@ var MongoClient= require('mongodb').MongoClient;
 var url= "mongodb+srv://admin:admin@cluster0.ebgh1.mongodb.net/mydb?retryWrites=true&w=majority";
 MongoClient.connect(url, function(err, db){
     if (err) throw err;
-    console.log("HIi");
+    console.log("hello");
     var dbo = db.db("mydb");
     var query = { address: "Park Lane 38" };
     dbo.collection("customers").find(query).toArray(function(err, result) {
